88d7b19e2f28e396519635ad522c54abc23f9f0c
trinanumana <31747072+trinanumana@users.noreply.github.com>
2017-11-13 14:39:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-function hover(e) {
-	e.setAttribute('src', '');
+function myFunction() {
+	var element=document.getElementById("cora1");
+	element.classList.remove("hidden");
 }
 
-function unhover(e) {
-	e.setAttribute('src'.'');
+function makeChange() {
+	var element=document.getElementById("cora1");
+	element.classList.add("hidden");
+}
+
+function melJones() {
+	var element=document.getElementById("regmom");
+	element.classList.remove("hidden");
+}
+
+function jonesMel() {
+	var element=document.getElementById("regmom");
+	element.classList.add("hidden");
+}
+
+function theCat() {
+	var element=document.getElementById("cat");
+	element.classList.remove("hidden");
+}
+
+function catGone() {
+	var element=document.getElementById("cat");
+	element.classList.add("hidden");
+}
+
+function otherMother() {
+	var element=document.getElementById("othermother");
+	element.classList.remove("hidden");
+}
+
+function motherOther() {
+	var element=document.getElementById("othermother");
+	element.classList.add("hidden");
+}
+
+function secondCat() {
+	var element=document.getElementById("cat2");
+	element.classList.remove("hidden");
+}
+
+function catSecond() {
+	var element=document.getElementById("cat2");
+	element.classList.add("hidden");
 }
