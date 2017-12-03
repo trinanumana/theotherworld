5a34cfc6c97270e69ace0c5a57d78c52fe16414a
trinanumana <31747072+trinanumana@users.noreply.github.com>
2017-12-02 18:15:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function showPara(e) {
 	document.getElementById("kittytalk4").innerHTML="You can tell how your cat is feeling by the way they're moving their tail.";
 }
 
+//AUDIO FROM http://www.orangefreesounds.com/category/sound-effects/animal-sounds/cat-meow-sound/
+
 function playMeow(e) {
 	var audio = new Audio('meowp3/meow1.mp3');
 	audio.play();
@@ -32,45 +34,4 @@ function playAudio(e) {
 function playMusic(e) {
 	var audio = new Audio('meowp3/meow4.mp3');
 	audio.play();
-}
-
-function myTabby() {
-	var element=document.getElementById("tcat");
-	element.classList.remove("hidden");
-}
-
-function makeTabby() {
-	var element=document.getElementById("tcat");
-	element.classList.add("hidden");
-	audio.pause("meowp3/meow1.mp3");
-}
-
-function myRussianb() {
-	var element=document.getElementById("rbcat");
-	element.classList.remove("hidden");
-}
-
-function makeRussianb() {
-	var element=document.getElementById("rbcat");
-	element.classList.add("hidden");
-}
-
-function myOrange() {
-	var element=document.getElementById("ocat");
-	element.classList.remove("hidden");
-}
-
-function makeOrange() {
-	var element=document.getElementById("ocat");
-	element.classList.add("hidden");
-}
-
-function myWhiteBrown() {
-	var element=document.getElementById("wcat");
-	element.classList.remove("hidden");
-}
-
-function makeWhiteBrown() {
-	var element=document.getElementById("wcat");
-	element.classList.add("hidden");
 }
