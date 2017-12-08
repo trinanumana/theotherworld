e4c53c5b1c974b1b5553646d2c0f862d430f444a
trinanumana <31747072+trinanumana@users.noreply.github.com>
2017-12-08 15:04:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // these four functions work by locating the ID of a certain element, in this case it's the dark boxes, and it adds text, aka the innerHTML, to the div in the HTML with the ID the function is focusing on.
+// if you wanted to make the JavaScript interact with both, HTML and CSS, you could use something along the lines of 'document.getElementById("x").classList.add("b");'
 function showText(e) {
 	document.getElementById("kittytalk").innerHTML="Cats sleep, on average, 13 to 14 hours a day.";
 }
@@ -15,7 +16,7 @@ function showPara(e) {
 	document.getElementById("kittytalk4").innerHTML="Cats show emotion by the way they move their tail.";
 }
 
-//this
+// this function removes the innerHTML from the dark boxs (by locating the IDs of each
 function resetInner(e) {
 	document.getElementById("kittytalk").innerHTML="";
 	document.getElementById("kittytalk2").innerHTML="";
@@ -24,8 +25,8 @@ function resetInner(e) {
 }
 
 //CODE FROM https://stackoverflow.com/questions/9419263/playing-audio-with-javascript
-//AUDIO FROM http://www.orangefreesounds.com/category/sound-effects/animal-sounds/cat-meow-sound/
-//these functions work when the element, img, is clicked on. in the HTML, the img has an 'onclick' and that commands the javascript to play the audio file that's specified in the singular function. For this, nothing from the CSS is being pulled or added.
+// AUDIO FROM http://www.orangefreesounds.com/category/sound-effects/animal-sounds/cat-meow-sound/
+// these functions work when the element, img, is clicked on. in the HTML, the img has an 'onclick' and that commands the javascript to play the audio file that's specified in the singular function. For this, nothing from the CSS is being pulled or added.
 function playMeow(e) {
 	var audio = new Audio('meowp3/meow1.mp3');
 	audio.play();
